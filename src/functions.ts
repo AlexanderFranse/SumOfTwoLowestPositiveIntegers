28ff3fdd79c09faa4912c5b46c930ebff6581232
alexander.franse <alexander.franse@ns.nl>
2024-10-15 10:10:19 +0200

    refactor: renamed validation function name

--- src/functions.ts
+++ src/functions.ts
@@ -1,4 +1,4 @@
-export function containArrayPositiveIntegers(x: number[]): boolean {
+export function validatePositiveIntegers(x: number[]): boolean {
   return x.every(
     (element) => element > 0 && Number.isSafeInteger(2.0) && element % 1 === 0
   );
--- test/unit/unittemplate.test.ts
+++ test/unit/unittemplate.test.ts
@@ -1,27 +1,24 @@
-import {
-  containArrayPositiveIntegers,
-  getLowestNumber,
-} from "../../src/functions";
+import { validatePositiveIntegers, getLowestNumber } from "../../src/functions";
 
 describe(`A method is needed to determine the sum of the two lowest positive numbers given an array of minimum 4 positive integers. 
   No floats or non-positive integers will be passed`, () => {
   describe("To achieve this, validation is needed to see if all integers are positive values", () => {
     describe("All integers are positive. Validation passsed!", () => {
       it("[19, 5, 42, 2, 77] --> return true", () => {
-        expect(containArrayPositiveIntegers([19, 5, 42, 2, 77])).toBe(true);
+        expect(validatePositiveIntegers([19, 5, 42, 2, 77])).toBe(true);
       });
     });
     describe("One integer contains float number. Validation failed", () => {
       it("[19, 5, 42, 2.0, 77] --> return false", () => {
         const floatNumber = 3.14;
         expect(
-          containArrayPositiveIntegers([19, 5, 42, floatNumber, 77])
+          validatePositiveIntegers([19, 5, 42, floatNumber, 77])
         ).toBeFalsy();
       });
     });
     describe("One integer contains 0. Validation failed", () => {
       it("[19, 5, 42, 0, 77] --> return false", () => {
-        expect(containArrayPositiveIntegers([19, 5, 42, 0, 77])).toBeFalsy();
+        expect(validatePositiveIntegers([19, 5, 42, 0, 77])).toBeFalsy();
       });
     });
   });
